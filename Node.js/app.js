2c7d6f848d78c81eb8e651acee9f66631f169804
Katie Duane <katiejduane@gmail.com>
2019-05-18 01:04:48 -0400

    sequelize with table relations, edit, destroy...

--- Node.js/app.js
+++ Node.js/app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
-const errorController = require('./controllers/error')
 
-const sequelize = require('./util/database')
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,6 +20,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user
+        next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -25,13 +36,35 @@ app.use(errorController.get404);
 
 //this is a special method that is aware of all your models and creates tables for them, 
 // and if you have them, relations
+
+Product.belongsTo(User, {
+    constraints: true,
+    //this means if the user is deleted, all of their products are deleted, too!
+    onDelete: 'CASCADE'
+}); //user createdproject
+
+User.hasMany(Product);
+
 sequelize
+    // .sync({force: true})
     .sync()
     .then(result => {
+        return User.findByPk(1);
         // console.log(result);
+        
+    })
+    .then(user => {
+        if(!user){
+            User.create({name: 'Katie', email: 'dummyemail@max.com'})
+        }
+        return Promise.resolve(user);
+        //returning Promise.resolve so that a .then can be chained, as 'user' is just
+        //a JS object, not a promise
+    })
+    .then(user => {
+        // console.log(user);
         app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
-});
-
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- Node.js/controllers/admin.js
+++ Node.js/controllers/admin.js
@@ -22,9 +22,10 @@ exports.postAddProduct = (req, res, next) => {
     .then( result => {
         // console.log(result)
         console.log("Created product");
+        res.redirect('/admin/products');
     })
     .catch(err => {
-        console.log(err)
+        console.log(err);
     })
     // const product = new Product(null, title, imageUrl, price, description);
     // product.save()
@@ -38,21 +39,23 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
     if (!editMode){
-        return res.redirect('/')
+        return res.redirect('/');
     }
     //the extracted value of  query param is always a string! so 'true' instead of true
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if(!product){
-            return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product,
-        });
-    }) 
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product,
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -61,29 +64,45 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDescription = req.body.description;
-    const updatedProduct = new Product(
-        prodId, 
-        updatedTitle, 
-        updatedImageUrl, 
-        updatedDescription, 
-        updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDescription;
+            product.imageUrl = updatedImageUrl;
+            return product.save();
+        })
+        //this additional .then() will use the return statement from above!
+        .then(result => {
+            console.log('updated product');
+            res.redirect('/admin/products');
+        })
+        //the .catch() will handle errors from both .then() statements!
+        .catch(err => console.log(err));
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-        });
-    });
+    Product.findAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            });
+        })
+        .catch(err => console.log(err))
+
 }
 
 exports.postDeleteProduct = (req,res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products')
+    Product.findByPk(prodId)
+    .then(product => {
+        return product.destroy();
+    })
+    .then(result => {
+        console.log('destroyed product');
+        res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 }
